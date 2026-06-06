0713f11f650f8947b723d4d513c4bfd989fce179
erikaramovich <erogalstyan19@gmail.com>
2026-06-07 01:26:35 +0400

    27: fix it using the two-pointer technique from previous problem #26

--- src/main/java/org/example/easy/RemoveElement_27.java
+++ src/main/java/org/example/easy/RemoveElement_27.java
@@ -4,26 +4,21 @@ import java.util.Arrays;
 
 public class RemoveElement_27 {
     public static void main(String[] args) {
-        System.out.println(removeElement(new int[]{3,2,2,3}, 3));
+        System.out.println(removeElement(new int[]{3, 2, 2, 3}, 3));
+        System.out.println(removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2));
     }
 
     public static int removeElement(int[] nums, int val) {
-        int occurence = 0;
-        int end = nums.length - 1;
+        int write = 0;
 
-
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
-            if(nums[i] == val )
-            {
-                occurence++;
-                int temp = nums[i];
-                nums[i] = nums[end];
-                nums[end] = temp;
-                end--;
+        for (int read = 0; read < nums.length; read++) {
+            // if the current element is not equal to the value, we save it
+            if (nums[read] != val) {
+                nums[write] = nums[read];
+                write++;
             }
         }
         System.out.println(Arrays.toString(nums));
-        return occurence;
-        }
+        return write;
     }
+}
